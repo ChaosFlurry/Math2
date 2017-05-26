9e2a0b08bfe81e01de903bbde0241d5df11517dd
ChaosFlurry <chaosflurry@gmail.com>
2017-05-25 23:53:44 -0700

    Updated test()

--- Math.py
+++ Math.py
@@ -9,7 +9,6 @@ def main():
 
 
 def test():
-    """
     start = time.time()
     for i in range(0, 1001):
         bernoulli = Utilities.bernoulli(i)
@@ -17,11 +16,12 @@ def test():
     end = time.time()
     elapsed = end - start
     print(str(elapsed) + " seconds")
-    print(Utilities.bernoulli(64).to_str())
+    #print(Utilities.bernoulli(64).to_str())
     """
     f = Function("ln(1+x)*sin(10x)")
     f = Function("44*(x-1)*(5*x-27)*(5*x-16)*(8*x-49)*(8*x-35)*(40*x-81)/4671275427")
     print(f.differentiate(6.4))
     print(f.integrate(0, 6.4))
+    """
 
 test()
